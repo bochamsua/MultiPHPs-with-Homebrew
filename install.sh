1fb5ee4dfaf949cb9f51a29f46ab40aca6a684f0
bochamsua <bphong@my.lonestar.edu>
2018-09-04 17:30:53 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,12 +10,33 @@ if xcode-select --install 2>&1 | grep installed; then
 
 	echo "---------------------------------------------------------------------"
 	echo "2. Unload pre-installed apache come with macOS"
-	sudo apachectl stop && sudo launchctl unload -w /System/Library/LaunchDaemons/org.apache.httpd.plist 2>/dev/null
+	sudo apachectl stop 2>/dev/null
+
+  echo "---------------------------------------------------------------------"
+	echo "3. Install Dnsmasq to auto resolve to localhost"
+	brew install dnsmasq
+	echo 'address=/.test/127.0.0.1' > /usr/local/etc/dnsmasq.conf
+	sudo brew services start dnsmasq
+  #sudo ln -sfv /usr/local/opt/dnsmasq/homebrew.mxcl.dnsmasq.plist /Library/LaunchDaemons/
+
+	sudo mkdir -v /etc/resolver
+	sudo bash -c 'echo "nameserver 127.0.0.1" > /etc/resolver/test'
+	sudo brew services restart dnsmasq
+  #sudo launchctl reload /Library/LaunchDaemons/homebrew.mxcl.dnsmasq.plist
+
+
+  echo "---------------------------------------------------------------------"
+	echo "4. Install MariaDB"
+	brew install mariadb
+	brew services start mariadb
+
+
+	/usr/local/bin/mysql_secure_installation
+
 
 	echo "---------------------------------------------------------------------"
-	echo "3. Install apache and process config files"
+	echo "5. Install apache and process config files"
 	brew install httpd
-	
 
 	cp /usr/local/etc/httpd/httpd.conf /usr/local/etc/httpd/httpd.conf.bak
 	sed -i '' "s|Listen 8080|Listen 80|g" /usr/local/etc/httpd/httpd.conf
@@ -34,83 +55,52 @@ if xcode-select --install 2>&1 | grep installed; then
 	cp -rf httpd-vhosts.conf /usr/local/etc/httpd/extra/httpd-vhosts.conf
 
 	echo "---------------------------------------------------------------------"
-	echo "4. Copy some scripts and include PATH"
+	echo "6. Copy some scripts and include PATH"
 	chmod +x fpm i o sphp v x
 	cp fpm i o sphp v x /usr/local/bin/
 	export PATH=/usr/local/sbin:$PATH
 
 	echo "---------------------------------------------------------------------"
-	echo "5. Install PHPs, xDebug, Tidy and process config files"
+	echo "7. Install PHPs, xDebug, Tidy and process config files"
 
 	mkdir -p ~/Library/LaunchAgents
+  brew tap kabel/php-ext
+  #brew install libyaml
 	for i in ${php_array[@]}
 		do
+      NODOT=${i/./}
 			brew install php@"$i"
+      cp /usr/local/etc/php/"$i"/php.ini /usr/local/etc/php/"$i"/php.ini.bak
+      cp -rf conf/* /usr/local/etc/php/"$i"/conf.d/
+
 			if [ "$i" == "7.2" ]; then
 				ln -sfv /usr/local/opt/php@"$i"/homebrew.mxcl.php.plist ~/Library/LaunchAgents/homebrew.mxcl.php@"$i".plist
-			else 
+        brew install kabel/php-ext/php-tidy
+        pecl install xdebug
+        sed -i '' "s/9000/90$NODOT/g" /usr/local/etc/php/"$i"/php-fpm.d/www.conf
+			else
 				ln -sfv /usr/local/opt/php@"$i"/homebrew.mxcl.php@"$i".plist ~/Library/LaunchAgents/
+        brew install kabel/php-ext/php@"$i"-tidy
+        if [[ "$i" == "5.6" ]]; then
+          pecl install xdebug-2.5.5
+          sed -i '' "s/9000/90$NODOT/g" /usr/local/etc/php/"$i"/php-fpm.conf
+        else
+          pecl install xdebug
+          sed -i '' "s/9000/90$NODOT/g" /usr/local/etc/php/"$i"/php-fpm.d/www.conf
+        fi
 			fi
+
+      sed -i '' 's/zend_extension=/;zend_extension=/g' /usr/local/etc/php/"$i"/php.ini
+  		sed -i '' 's/extension=/;extension=/g' /usr/local/etc/php/"$i"/php.ini
+  		sed -i '' 's/max_execution_time = 30/max_execution_time = 30000/g' /usr/local/etc/php/"$i"/php.ini
+  		sed -i '' 's/max_execution_time = 30/max_execution_time = 30000/g' /usr/local/etc/php/"$i"/php.ini
+  		sed -i '' 's/max_input_time = 60/max_input_time = 60000/g' /usr/local/etc/php/"$i"/php.ini
+  		sed -i '' 's/memory_limit = 128M/memory_limit = 512M/g' /usr/local/etc/php/"$i"/php.ini
+  		sed -i '' 's/post_max_size = 8M/post_max_size = 128M/g' /usr/local/etc/php/"$i"/php.ini
+  		sed -i '' 's/upload_max_filesize = 2M/upload_max_filesize = 128M/g' /usr/local/etc/php/"$i"/php.ini
 
 			launchctl load ~/Library/LaunchAgents/homebrew.mxcl.php@"$i".plist
 	done
-
-	
-	find /usr/local/etc/php/ -type f -name "*.ini" -exec cp {} {}.bak \;
-
-
-	#brew install libyaml
-	brew tap kabel/php-ext
-
-	for i in ${php_array[@]}
-		do
-			NODOT=${i/./}
-			sphp "$i"
-
-			if [ "$i" == "7.2" ]; then
-				brew install kabel/php-ext/php-tidy
-			else 
-				brew install kabel/php-ext/php@"$i"-tidy
-			fi
-
-			#pecl install yaml
-			cp -rf conf/* /usr/local/etc/php/"$i"/conf.d/
-			if [ "$i" == "5.6" ]; then
-				pecl install xdebug-2.5.5
-				sed -i '' "s/9000/90$NODOT/g" /usr/local/etc/php/"$i"/php-fpm.conf
-			else 
-				pecl install xdebug
-				sed -i '' "s/9000/90$NODOT/g" /usr/local/etc/php/"$i"/php-fpm.d/www.conf
-			fi
-		sed -i '' 's/zend_extension=/;zend_extension=/g' /usr/local/etc/php/"$i"/php.ini
-		sed -i '' 's/extension=/;extension=/g' /usr/local/etc/php/"$i"/php.ini
-	
-		sed -i '' 's/max_execution_time = 30/max_execution_time = 30000/g' /usr/local/etc/php/"$i"/php.ini
-		sed -i '' 's/max_execution_time = 30/max_execution_time = 30000/g' /usr/local/etc/php/"$i"/php.ini
-		sed -i '' 's/max_input_time = 60/max_input_time = 60000/g' /usr/local/etc/php/"$i"/php.ini
-		sed -i '' 's/memory_limit = 128M/memory_limit = 512M/g' /usr/local/etc/php/"$i"/php.ini
-		sed -i '' 's/post_max_size = 8M/post_max_size = 128M/g' /usr/local/etc/php/"$i"/php.ini
-		sed -i '' 's/upload_max_filesize = 2M/upload_max_filesize = 128M/g' /usr/local/etc/php/"$i"/php.ini
-
-
-	done
-	
-	echo "---------------------------------------------------------------------"
-	echo "6. Install MariaDB"
-	brew install mariadb
-	brew services start mariadb
-	/usr/local/bin/mysql_secure_installation
-
-	
-	echo "---------------------------------------------------------------------"
-	echo "7. Install Dnsmasq to auto resolve to localhost"
-	brew install dnsmasq
-	echo 'address=/.test/127.0.0.1' > /usr/local/etc/dnsmasq.conf
-	sudo brew services start dnsmasq
-
-	sudo mkdir -v /etc/resolver
-	sudo bash -c 'echo "nameserver 127.0.0.1" > /etc/resolver/test'
-	sudo brew services restart dnsmasq
 
 	echo "---------------------------------------------------------------------"
 	echo "8. Add some aliases to .bash_profile"
@@ -123,21 +113,16 @@ if xcode-select --install 2>&1 | grep installed; then
 		alias m.o='brew services stop mariadb'
 		alias m.r='brew services restart mariadb'
 
-		alias a.conf='subl /usr/local/etc/httpd/httpd.conf'
-		alias v.conf='subl /usr/local/etc/httpd/extra/httpd-vhosts.conf'
+		alias a.conf='atom /usr/local/etc/httpd/httpd.conf'
+		alias v.conf='atom /usr/local/etc/httpd/extra/httpd-vhosts.conf'
 EOF
 	source ~/.bash_profile
 
-
-
 	echo "---------------------------------------------------------------------"
 	echo "Retart all services"
-	sudo brew services restart httpd
+	a.r
 	fpm
-	brew services restart mariadb
-
-
-
+	m.r
 
 	echo "Finished."
 else
@@ -145,10 +130,3 @@ else
    xcode-select --install
 
 fi
-
-
-
-
-
-
-
